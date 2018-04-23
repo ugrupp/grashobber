a1fffc6c302645003385188580f5ca7d595ee770
Urs Grupp <urs.grupp@gmail.com>
2018-04-23 14:09:59 +0200

    Initial commit: Frontend workflow, project structure.

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+//
+// JS entry file
+// --------------------------------------------------
+
+// SVG <use> polyfill
+import 'svgxuse';
+
+// Babel polyfill
+// This import will transpile to single core-js module imports. Only the polyfills needed for our target browsers will be imported.
+// Powered by `useBuiltIns: usage` in .babelrc
+import 'babel-polyfill'; // eslint-disable-line import/no-unresolved
+
+// module imports
+import Testmodule from './modules/testmodule';
+
+// init modules
+let testmodule = new Testmodule();
+testmodule.logMe();
