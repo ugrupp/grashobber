4a83ff5b6189585832ee922e7c961e585c4b6c6e
Urs Grupp <urs.grupp@gmail.com>
2018-05-04 09:54:21 +0200

    Adjusted menu hover

--- src/js/modules/reveal-animations.js
+++ src/js/modules/reveal-animations.js
@@ -2,9 +2,6 @@
 // scrollreaveal should be a window variable, set in the <head> script
 
 if (typeof window.sr !== 'undefined') {
-  // Disable reveal on pageload
-  // https://github.com/jlmakes/scrollreveal/issues/276
-
   window.sr.reveal('[data-reveal]', {
     duration: 750,
     distance: '3em',
